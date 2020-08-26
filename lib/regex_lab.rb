edb497842bd6edacef427e0d1c7f260cf0f18a66
Christerle Nounoute <christerlesn@gmail.com>
2020-08-26 18:21:50 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,12 +7,11 @@ end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-text.scan(/un\w+ing/)
+
 end
 
 def words_five_letters_long(text)
-text.scan(\w{5})
-end
+
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
