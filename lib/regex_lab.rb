90bc32bc7182b08f2d5cc68e2c16a1a9013ef1c5
Christerle Nounoute <christerlesn@gmail.com>
2020-08-26 18:24:12 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -12,7 +12,7 @@ end
 
 def words_five_letters_long(text)
 words_array = text.split(" ")
-words_array.gerp(/{5}/)
+words_array.gerp(/\w{5}/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
